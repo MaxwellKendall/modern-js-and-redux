a653a60353690b1fd5ceb9012f0a74f0cf15a4f0
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2017-08-16 12:03:43 -0400

    this is an example of how state works

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
+const API_KEY = 'AIzaSyDeGJDQOeernziYoE5OSNOyFrv2hI8tyZ0';
 
-import App from './components/app';
-import reducers from './reducers';
+// (1) create a new component that produces some html
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const App = () => {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    )
+}
+
+
+// (2) Take the components generated html and put it on the page (in the DOM)
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    <App />,
+    document.querySelector('.container')
+)
